6f393768b5a78ab9a9cf7cb2c6320d7cfd7928e1
Miguel Brandão <114339832+MiguelbProductions@users.noreply.github.com>
2024-05-11 19:25:02 +0100

    Position Class Added

--- src/Application/Program.java
+++ src/Application/Program.java
@@ -1,7 +1,10 @@
 package Application;
 
+import BoardGame.Position;
+
 public class Program {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        Position position = new Position(1, 1);
+        System.out.println(position);
     }
 }
